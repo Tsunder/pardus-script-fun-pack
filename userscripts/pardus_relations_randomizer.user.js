27f95d53be80017a87c354c2281937b90dc976e1
Tsunder <Tsunder@users.noreply.github.com>
2018-11-29 21:02:32 -0500

    lol

--- userscripts/pardus_relations_randomizer.user.js
+++ userscripts/pardus_relations_randomizer.user.js
@@ -12,8 +12,69 @@
 
 (function() {
     'use strict';
+
     randomChanges();
-    //let table = document.querySelector('td[title*="Federation"]').parentElement.parentElement.parentElement.parentElement.parentElement.parentElement
+    //randomInfluences();
+    //randomLobbying();
+    randomDispositions();
+    randomRelations();
+
+    function randomRelations() {
+        let federation = document.querySelectorAll('[title="Federation"]')
+        let empire = document.querySelectorAll('[title="Empire"]')
+        let union = document.querySelectorAll('[title="Union"]')
+
+        let relations = [
+        [federation[0], empire[0]],
+        [empire[1], union[0]],
+        [union[1], federation[1]]
+        ]
+        for (var _row in relations) {
+            let position = Math.floor(Math.random()*416);
+            //console.log(relations[_row][0].parentElement.nextElementSibling.querySelector("tr"))
+            for (var _ele in relations[_row]) {
+                relations[_row][_ele].previousElementSibling.width=position
+                relations[_row][_ele].nextElementSibling.width=416-position
+            }
+        }
+    }
+
+    function randomInfluences() {
+        let diplomacyTable = document.querySelector('td[title*="Federation"]').parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.children
+    }
+
+    //partially overlaps with randomDispositions
+    function randomLobbying() {
+        let _largeSign = [ "//static.pardus.at/img/std/disposition_plus.png", "//static.pardus.at/img/std/disposition_neutral.png", "//static.pardus.at/img/std/disposition_minus.png"];
+        document.querySelectorAll('th+td[colspan="2"]').forEach( function (e) {
+            e.children[0].src = _largeSign[Math.floor(Math.random()*3)]
+        })
+    }
+
+    function randomDispositions() {
+        let _largeSign = [ "//static.pardus.at/img/std/disposition_plus.png", "//static.pardus.at/img/std/disposition_neutral.png", "//static.pardus.at/img/std/disposition_minus.png"];
+        let _colors = ["#8C8C8C", "#E70000", "#06E800"]; //neutral, negative, positive
+        let _signSrc = [ "//static.pardus.at/img/std/disposition_single_neutral.png" , "//static.pardus.at/img/std/disposition_single_plus.png", "//static.pardus.at/img/std/disposition_single_minus.png"]
+        Array.from(document.querySelectorAll('span[style*="font-size:8px"]')).forEach( function (e) {
+            var _disposition = Math.floor(Math.random()*3)* Math.floor(Math.random()*2)
+            e.style.color = _colors[_disposition];
+            e.parentElement.previousElementSibling.children[0].src = _signSrc[_disposition];
+        });
+
+        Array.from(document.querySelectorAll('td[width="30"]>span[style*="font-size:16px;color:"]')).forEach(function(e){
+            e.innerText= "?"})
+        Array.from(document.querySelectorAll('th[colspan="2"]~td[align="center"]~td[align="center"]')).forEach(function(e) {
+            console.log(e)
+            e.children[0].innerText = "?";
+            e.previousElementSibling.children[0].innerText = "?";
+        });
+        document.querySelectorAll('img[title*="Disposition"]').forEach(function(e){
+            if(e.parentElement) {
+                e.parentElement.innerHTML='<img src="' + _largeSign[Math.floor(Math.random()*3)] +'" height="16" width="16" alt="Indifferent Disposition" title="Indifferent Disposition">'
+            }
+        })
+    }
+
 
     function randomChanges(){
         let colorList = ["#E70000","#8C8C8C","#06E800"] // negative, neutral , positive
