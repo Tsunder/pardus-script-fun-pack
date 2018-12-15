07c3163ac5c4bda577472d9c8077bf23448c1c0e
Tsunder <Tsunder@users.noreply.github.com>
2018-12-14 19:34:39 -0500

    increase resolution of harrier, various changes
    makes donate button an addition to existing drop button.
    co-incidentally, this also is a workaround to undonateable materials, since the drop functionality is still there.

--- userscripts/pardus_holiday_drop.user.js
+++ userscripts/pardus_holiday_drop.user.js
@@ -1,36 +1,33 @@
 // ==UserScript==
 // @name         Drop To Donate
 // @namespace    Tsunders
-// @version      0.2
-// @description  When you drop stuff, you donate it instead! ^_^ Remember to disable this once the holiday drive is over. Also disable if you want to drop undonatable stuff like illegal commodities.
+// @version      0.3
+// @description  Adds a Donate button to your drop cargo screen! ^_^ Remember to disable this once the holiday drive is over!
 // @author       Tsunders
 // @match        *://*.pardus.at/drop_cargo.php*
 // @grant        none
-// @icon         http://static.pardus.at/img/stdhq/96/ships/harrier_xmas.png
+// @icon         http://static.pardus.at/img/stdhq/128/ships/harrier_xmas.png
 // @updateURL    https://github.com/Tsunder/pardus-script-fun-pack/raw/master/userscripts/pardus_holiday_drop.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    document.querySelector('form[action="drop_cargo.php"]').action = "xmas_charity.php"
-    document.querySelector('input[name="drop"]').name = "donate";
-
-    function addUndonateButton() {
-	    let donateButton = document.getElementsByName("donate")[0];
-        let undonateButton = document.createElement("input");
-        let undonateSpan = document.createElement("span");
-        undonateButton.id = "undonate";
-        undonateButton.type = "button";
-	    undonateButton.value = "Undonate";
-	    undonateButton.addEventListener('click', undonate, false);
-	    undonateButton.style = donateButton.getAttribute("style");
-        undonateSpan.appendChild(undonateButton);
-	    donateButton.parentNode.insertBefore(undonateSpan,donateButton.parentNode.lastChild.previousElementSibling);
+    function addDonateButton() {
+	    let dropButton = document.getElementsByName("drop")[0];
+        let donateButton = document.createElement("input");
+        let donateSpan = document.createElement("span");
+        donateButton.id = "donate";
+        donateButton.name = "donate";
+        donateButton.type = "submit";
+	    donateButton.value = "Donate";
+	    donateButton.addEventListener('click', donate, false);
+	    donateButton.style = dropButton.getAttribute("style");
+        donateSpan.appendChild(donateButton);
+	    dropButton.parentNode.insertBefore(donateSpan,dropButton.parentNode.lastChild.previousElementSibling);
 	}
-    addUndonateButton();
-    function undonate() {
-            document.querySelector('form[action="xmas_charity.php"]').action = "drop_cargo.php"
-    document.querySelector('input[name="donate"]').name = "drop";
+    addDonateButton();
+    function donate() {
+        document.querySelector('form[action="drop_cargo.php"]').action = "xmas_charity.php"
     }
 })();
