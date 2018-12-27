fb4b89ee2ecad36f913a44f19ac35321658842d6
Tsunder <Tsunder@users.noreply.github.com>
2018-12-27 00:40:06 -0500

    Update pardus_shadow_highbeams.user.js

--- userscripts/pardus_shadow_highbeams.user.js
+++ userscripts/pardus_shadow_highbeams.user.js
@@ -3,22 +3,31 @@
 // @namespace   Astraltoremail@gmail.com
 // @author      Tsunders
 // @description Shines a light on shadows so you don't miss them.
-// @include     http*://*.pardus.at/main.php*
-// @version     1.1
+// @include     *://*.pardus.at/main.php*
+// @version     1.2
 // @grant       none
 // @updateURL 	https://github.com/Tsunder/pardus-script-fun-pack/raw/master/userscripts/shadow_highbeams.user.js
 // @downloadURL https://github.com/Tsunder/pardus-script-fun-pack/raw/master/userscripts/shadow_highbeams.user.js
 // @icon 		http://static.pardus.at/img/std/opponents/shadow.png
 // ==/UserScript==
 
+// 1.2 2018 december 27 partial refresh??
 // 1.1 2018 november 28 download migration
 // 1.0 2018 november 08 initial creation
 
-function HighlightShadows() {
-    var shadows = document.querySelectorAll('#navarea a > img[src*=shadow]');
-    for (var i = 0; i < shadows.length; i++) {
-        shadows[i].parentNode.setAttribute('style', 'background:#00B448');
+var HighlightShadows = {
+    Init : function() {
+        console.log("first run");
+        Array.from(document.querySelectorAll('#navarea a > img[src*=bio]')).forEach(e => {
+            e.parentNode.setAttribute('style', 'background:#00B448')}
+                                                                                      )
+},
+    Update: function() {
+        console.log("update");
+        Array.from(document.querySelectorAll('#navarea a > img[src*=bio]')).forEach(e => {
+            e.parentNode.setAttribute('style', 'background:#00B448')}
+                                                                                      )
     }
 }
-
-HighlightShadows();
+HighlightShadows.Init();
+window.addUserFunction(function check(e) {return function() {e.Update()}}(HighlightShadows));
