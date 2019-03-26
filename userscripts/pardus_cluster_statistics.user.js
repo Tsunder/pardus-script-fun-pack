603d971f3252189d10aa387c305f6f16ca42c7c3
Tsunder <Tsunder@users.noreply.github.com>
2019-03-26 00:16:13 -0400

    Update pardus_cluster_statistics.user.js

--- userscripts/pardus_cluster_statistics.user.js
+++ userscripts/pardus_cluster_statistics.user.js
@@ -19,7 +19,7 @@
     let populationDecreaseColor = '#D21414'; // red
     let universe = window.location.host.substr(0, window.location.host.indexOf('.'));
 
-    let lowPopThreshold = 1001;
+    let lowPopThreshold = 1001; // 1000 pop is about 12 ticks (1.5 days) before downgrade. 1500 about 16 ticks (2 days). 850 about 8 ticks (1 day)
     let lowPopFontSize = "24px"; // 2x as large as regular
     let lowPopColour = '#FFA500'; // orange
     let healthy = true;
