e32a78bedc731ea269310e81b0b3e8cc22abb0bc
Tsunder <Tsunder@users.noreply.github.com>
2019-04-05 00:01:38 -0400

    Update pardus_chat_links.user.js

--- userscripts/pardus_chat_links.user.js
+++ userscripts/pardus_chat_links.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name           Pardus chat links
 // @namespace      pardus.at
-// @author         Lucky, (C) 2009-2013, tsunders 2018
-// @version        0.2.2
+// @author         Lucky, (C) 2009-2013, tsunders 2019
+// @version        0.2.3
 // @description    Pardus chat links
 // @downloadurl    https://github.com/Tsunder/pardus-script-fun-pack/raw/master/userscripts/pardus_chat_links.user.js
 // @updateurl      https://github.com/Tsunder/pardus-script-fun-pack/raw/master/userscripts/pardus_chat_links.user.js
@@ -34,7 +34,7 @@ function chatLinks() {
             if(thisline.innerHTML.search('acs_log') == -1)
             {
                 var replacementString = thisline.innerHTML.replace(/\s(http|https|ftp|www)(:\/\/|\.)([^<\s]+)/gi,
-                                                                   '<a href="$1$2$3" target="_blank">$1$2$3<\/a>');
+                                                                   ' <a href="$1$2$3" target="_blank">$1$2$3<\/a> ');
                 if (thisline.innerHTML.localeCompare(replacementString) != 0)
                 {
                     thisline.innerHTML = replacementString;
