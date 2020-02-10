7cf4ef720689e5ba215cd191448f7300df4bb4e0
Tsunder <Tsunder@users.noreply.github.com>
2020-02-10 16:57:32 -0500

    Create pardus_Always Show Sector influence.user.js

---
+++ userscripts/pardus_Always Show Sector influence.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Always Show Sector influence
+// @namespace    Pardus
+// @version      0.1
+// @description  Automatically shows the alliance influence on planets/starbases.
+// @author       Tsunder
+// @match        *.pardus.at/planet.php*
+// @match        *.pardus.at/starbase.php*
+// @downloadURL
+// @updateURL
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    showAllianceRanking();
+    // Your code here...
+})();
