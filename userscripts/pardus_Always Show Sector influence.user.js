5164217d90d8fedafd85e360873ff94dd354db83
Tsunder <Tsunder@users.noreply.github.com>
2020-02-10 16:58:06 -0500

    add url

--- userscripts/pardus_Always Show Sector influence.user.js
+++ userscripts/pardus_Always Show Sector influence.user.js
@@ -6,8 +6,8 @@
 // @author       Tsunder
 // @match        *.pardus.at/planet.php*
 // @match        *.pardus.at/starbase.php*
-// @downloadURL
-// @updateURL
+// @downloadURL  https://github.com/Tsunder/pardus-script-fun-pack/raw/master/userscripts/pardus_Always%20Show%20Sector%20influence.user.js
+// @updateURL    https://github.com/Tsunder/pardus-script-fun-pack/raw/master/userscripts/pardus_Always%20Show%20Sector%20influence.user.js
 // @grant        none
 // ==/UserScript==
 
