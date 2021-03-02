5cd37b85149b5de095bc10b7a02369acc1cd0da6
Tsunder <Tsunder@users.noreply.github.com>
2021-03-02 01:51:50 -0500

    update pardus mag scoop
    now uses mutation observer, maybe this is heavy? lol who CARES

--- userscripts/pardus_mag_scoop_warning.user.js
+++ userscripts/pardus_mag_scoop_warning.user.js
@@ -1,31 +1,26 @@
 // ==UserScript==
 // @name         Mag Scoop Warning
-// @namespace    https://github.com/Tsunder/
-// @version      0.2
-// @description  Adds visual warning for having cargo in magnetic scoop.
-// @author       Tsunder
-// @match        http*://*.pardus.at/main.php*
-// @grant        none
+// @namespace    Pardus
+// @version      0.3
+// @description  displays mag scoop warning
+// @author       Tsunders
+// @match        *.pardus.at/main.php*
 // @downloadURL  https://github.com/Tsunder/pardus-script-fun-pack/raw/master/userscripts/pardus_mag_scoop_warning.user.js
 // @updateURL    https://github.com/Tsunder/pardus-script-fun-pack/raw/master/userscripts/pardus_mag_scoop_warning.user.js
-
+// @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    /*
-    const cargobox = document.getElementById("cargo_content")
-    if (!cargobox) {
+    if (!(document.getElementById("cargo"))) {
         return;
-    }*/
-    //const cargotext = cargobox.lastChild.firstChild;
+    }
+    const MutationObserver = window.MutationObserver;
+    const observer = new MutationObserver(run);
+    const config = { attributes: false, childList: true, subtree: true };
 
-
-    //if (cargobox.innerText.indexOf("t in magnetic")>-1) {
-        //addmag(cargobox)
-    //}
-
+    var target = document.querySelector("#cargo")
+    observer.observe(target,config);
     function addmag(cargobox) {
         let magwarning = document.createElement("div");
         magwarning.id="magwarning";
@@ -34,12 +29,9 @@
         cargobox.parentElement.append(magwarning);
     }
 
-    //maybe works??
-    //document.onreadystatechange = () { if (document.readyState != "complete") {return;} run();}
     function run() {
-        //console.log("complete");
+        console.log("running");
         let cargobox = document.getElementById("cargo_content")
-        if (!cargobox) {return;}
         if (cargobox.innerText.indexOf("t in magnetic")>0) {
             let magwarning = document.getElementById("magwarning")
             if (!magwarning) {
@@ -56,41 +48,4 @@
         }
 
     }
-    run();
-
-   /*function mutationHandler(mutationList) {
-       mutationList.forEach(function(mutation) {
-           if (mutation.type == "attributes") {
-
-                var cargobox = document.getElementById("cargo_content")
-                if (!cargobox) {
-                    return;
-                }
-               console.log("mutating!");
-                if (cargobox.innerText.indexOf("t in magnetic")>0) {
-                    if(document.getElementById("magwarning")) {
-                        magwarning.setAttribute("display","");
-                    }
-                    else {
-                        cargobox.parentElement.append(magwarning);
-                    }
-                }
-                else {
-                    magwarning.setAttribute("display","none");
-                }
-            }
-            else {
-                //console.log(mutation);
-            }
-        });
-    };
-    const MutationObserver = window.MutationObserver;
-    const observer = new MutationObserver(mutationHandler);
-    const config = { attributes: true, childList: true, subtree: true };
-    var target = document.querySelector("#cargo_content");//should be identical to cargobox??
-    */
-    //observer.observe(target,config);
-
-    //console.log("mutation observer made and running");
-
 })();
