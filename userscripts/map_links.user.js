ea78fb64ef721abacf7aefb6f46e787fb901da3c
Tsunder <Tsunder@users.noreply.github.com>
2021-03-24 23:39:42 -0400

    Update and rename pardus_combat_log_map_links.user.js to map_links.user.js

--- userscripts/map_links.user.js
+++ userscripts/map_links.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
-// @name         Combat Log Sector Links
+// @name         Map Linkz
 // @namespace    Pardus
-// @version      0.1.4
+// @version      0.1.5
 // @description  Adds links to the sector map of combat logs
 // @author       Tsunders
 // @updateURL    https://github.com/Tsunder/pardus-script-fun-pack/raw/master/userscripts/pardus_combat_log_map_links.user.js
 // @downloadURL  https://github.com/Tsunder/pardus-script-fun-pack/raw/master/userscripts/pardus_combat_log_map_links.user.js
 // @match        http*://*.pardus.at/overview_combat_log.php*
 // @match        http*://*.pardus.at/combat_details.php*
+// @match        *.pardus.at/acs_log.php*
 // @grant        none
 // @icon         http://www.pardus.at/favicon.ico
 
-// todo: add option to use xcom or pardusmapper
-
+// 0.1.5 add ACS logs, rename
 // 0.1.4 migrate to map.xcom and add coord highlighting
 // 0.1.3 download migration and icon
 // 0.1.2.2 url fix
@@ -66,6 +66,24 @@ var combatLogFluff = "Confrontation in "; //combat log fluff text before the sec
                 var _details = document.getElementsByTagName("h2")[0];
                 _details.append(document.createElement("br"));
                 _details.append(_sectorLink);
+            }
+        }
+    }
+    else if (document.URL.indexOf('pardus.at/acs_log.php') > -1) {
+        var rows = Array.from(document.getElementsByTagName("tr"));
+        if (rows.length > 9) {
+            //cut it down to just the fun messages
+            rows.pop();
+            rows.splice(0,8);
+            var sector = "";
+            var coords =""
+            var url = "";
+            for (var i in rows) {
+                sector = rows[i].children[2]
+                coords = rows[i].children[3]
+                url = `${baseURL}/${sector.innerText}.html?x=${/(\d+)/.exec(coords.innerText)[0]}&y=${/(\d+)(?=])/.exec(coords.innerText)[0]}`;
+                sector.innerHTML = `<a target="_blank" href="${url}"> ${sector.innerText}</a>`
+               coords.innerHTML = `<a target="_blank" href="${url}"> ${coords.innerText}</a>`
             }
         }
     }
