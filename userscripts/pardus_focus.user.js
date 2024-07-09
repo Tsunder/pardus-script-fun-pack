e6b1b3c8ed1cb89dc926d0f12677dd2adbe4d88d
Tsunder <Tsunder@users.noreply.github.com>
2024-07-09 11:46:29 -0400

    Create pardus_focus.user.js
    bandage fix userscript for keybinds

---
+++ userscripts/pardus_focus.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Pardus Focus
+// @namespace    https://github.com/Tsunder/pardus-script-fun-pack
+// @version      2024-07-09
+// @description  literally just focuses pardus on page load, minus the message frame. temporary bandage until other keybind scripts are fixed
+// @author       You
+// @match        *.pardus.at/*
+// @exclude      *.pardus.at/msgframe.php*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=pardus.at
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.focus() // lmao
+})();
